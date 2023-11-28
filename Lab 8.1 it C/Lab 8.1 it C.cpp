f19402583caf1b791c6886e434f6cfb3a7117bb7
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-28 20:19:26 +0200

    Add project files.

---
+++ Lab 8.1 it C/Lab 8.1 it C.cpp
@@ -0,0 +1,67 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int FindChar(char* str)
+{
+    if (strlen(str) < 3)
+        return 0;
+    int k = 0;
+    for (int i = 1; i < strlen(str); i++) {
+        if (str[i] == 'B' || str[i] == 'A' || str[i] == 'S' || str[i] == 'I' || str[i] == 'C')
+            k++;
+    }
+    return k;
+}
+char* Change(char* str)
+{
+    size_t len = strlen(str);
+    if (len < 5)
+        return str;
+
+    char* tmp = new char[len * 2 + 1];
+    char* t = tmp;
+    tmp[0] = '\0';
+    size_t i = 0;
+
+    while (i < len)
+    {
+
+        if (strncmp(str + i, "BASIC", 5) == 0)
+        {
+            strcat(t, "DELPHI");
+            t += 6;
+            i += 5;
+        }
+        else
+        {
+            *t++ = str[i++];
+            *t = '\0';
+        }
+    }
+
+
+    while (i < len)
+    {
+        *t++ = str[i++];
+        *t = '\0';
+    }
+
+    *t = '\0';
+
+    strcpy(str, tmp);
+
+    return tmp;
+}
+int main()
+{
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+    cout << "Number of chars BASIC:    " << FindChar(str) << endl;
+    char* dest = new char[151];
+
+    dest = Change(str);
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << dest << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 8.1 it C/Lab 8.1 it C.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        TEST_METHOD(FindCharTest)
+        {
+            char str[6] = "BASIC";
+            int result = FindChar(str);
+            Assert::AreEqual(result, 4);
+        }
+    };
+}
